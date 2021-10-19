ddf682eeac7502e0425a1bfe355b506ba68a4db6
Tom Hicks <hickst@tohono.com>
2021-10-19 15:37:56 -0700

    Rename program in setup.

--- setup.py
+++ setup.py
@@ -2,14 +2,14 @@ from setuptools import setup, find_packages
 
 setup(
     name='intend4',
-    version='0.0.2',
+    version='0.2.0',
     packages=find_packages(),
     # package_data={'intend4': []},
     # include_package_data=True,
     install_requires=[],
     entry_points={
         'console_scripts': [
-            'add_intended4=intend4.add_intended4:main',
+            'intended4=intend4.intended4_cli:main',
         ]
     },
 )
