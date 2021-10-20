91926955e803c5b9f49f3301ceee99fe2dc44d8f
Tom Hicks <hickst@tohono.com>
2021-10-19 19:55:17 -0700

    Add a TEST_DATA_DIR constant.

--- tests/__init__.py
+++ tests/__init__.py
@@ -3,11 +3,14 @@ import os
 from config.settings import APP_ROOT
 
 TEST_DIR = None
+TEST_DATA_DIR = None
 TEST_DBCONFIG_FILEPATH = None
 
 if (os.environ.get('RUNNING_IN_CONTAINER') is not None):
     TEST_DIR = f"{APP_ROOT}/tests"
     TEST_RESOURCES_DIR = f"{TEST_DIR}/resources"
+    TEST_DATA_DIR = f"{TEST_RESOURCES_DIR}/data"
 else:
     TEST_DIR = f"{os.getcwd()}/tests"
     TEST_RESOURCES_DIR = f"{TEST_DIR}/resources"
+    TEST_DATA_DIR = f"{TEST_RESOURCES_DIR}/data"
