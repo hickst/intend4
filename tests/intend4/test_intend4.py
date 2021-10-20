69497b833117b51149da44466584221e433395bb
Tom Hicks <hickst@tohono.com>
2021-10-19 19:55:42 -0700

    Add test for get_permissions.

--- tests/intend4/test_intend4.py
+++ tests/intend4/test_intend4.py
@@ -1,12 +1,12 @@
 # Tests of the IntendedFor module.
 #   Written by: Tom Hicks and Dianne Patterson. 10/19/2021.
-#   Last Modified: Initial creation.
+#   Last Modified: Add test for get_permissions.
 #
 import os
 import pytest
 import tempfile
 
-from tests import TEST_RESOURCES_DIR
+from tests import TEST_DATA_DIR
 import intend4.intend4 as in4
 
 
@@ -18,5 +18,11 @@ def popdir(request):
 
 class TestIntend4(object):
 
-  def test_dummy(self):
-    return True
+  dsdescr_fyl = f"{TEST_DATA_DIR}/dataset_description.json"
+
+  def test_get_permissions(self):
+    perms = in4.get_permissions(self.dsdescr_fyl)
+    print(perms)
+    assert perms is not None
+    assert type(perms) == int
+    assert perms == 33188
