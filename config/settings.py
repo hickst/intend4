4549ca7dbdbad2be7769b496428bf4acdeae5965
Tom Hicks <hickst@tohono.com>
2021-10-19 19:27:55 -0700

    Setup config package.

---
+++ config/settings.py
@@ -0,0 +1,8 @@
+# Path to the root location of the application inside the container.
+APP_ROOT = '/intend4'
+
+# Configuration directory, inside the application
+CONFIG_DIR = "{}/config".format(APP_ROOT)
+
+# Logging level
+LOG_LEVEL = 'INFO'  # CRITICAL / ERROR / WARNING / INFO / DEBUG
