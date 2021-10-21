6b5a7aee07920b0877656ca2ae3511bd26fb00c0
Tom Hicks <hickst@tohono.com>
2021-10-21 14:07:58 -0700

    Update version in setup.

--- setup.py
+++ setup.py
@@ -2,14 +2,14 @@ from setuptools import setup, find_packages
 
 setup(
     name='intend4',
-    version='0.2.0',
+    version='0.3.0',
     packages=find_packages(),
     # package_data={'intend4': []},
     # include_package_data=True,
     install_requires=[],
     entry_points={
         'console_scripts': [
-            'intend4=intend4.intend4_cli:main',
+            'intend4 = intend4.intend4_cli:main',
         ]
     },
 )
