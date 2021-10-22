a42331ffee02da1e2120c37755647697ee8a3533
Tom Hicks <hickst@tohono.com>
2021-10-21 18:56:34 -0700

    Rename distribution, update version to 0.4.0.

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='intend4',
-    version='0.3.0',
+    name='Intend4',
+    version='0.4.0',
     packages=find_packages(),
     # package_data={'intend4': []},
     # include_package_data=True,
